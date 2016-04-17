5d9ad30a4273c3182dbfc57eb1bf212d360685ca
[fengyuxili] <805219824@qq.com>
2016-04-17 14:24:18 +0800

    add bubble sort algorithm

---
+++ bubbleSort/bubbleSort.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+/*bubble sort is uesd to few data sort*/ 
+void bubbleSort(int list[] , int n);
+
+int main()
+{   
+    
+    int a[] = {2,4,6,8,1,3,5,7,9,10};
+	
+	bubbleSort(a,10);
+	for(int i=0;i<10;i++)
+	    cout<<a[i]<<" ";
+	cout<<endl;
+    return 0;
+}
+void bubbleSort(int list[] , int n)
+{
+    for(int i=0;i<n-1;i++)
+	    for(int j=0;j<n-i-1;j++){
+	    	if(list[j] > list[j+1])
+	    	    std::swap(list[j] ,list[j+1]);
+		}
+			
+} 
